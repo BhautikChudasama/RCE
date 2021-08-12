670f18cb62d0e2b33e36550df59e486b8b3d1b34
Bhautik ✨ <bhautikrchudasama@gmail.com>
2021-08-12 12:57:13 +0530

    Update server.test.js

--- test/server.test.js
+++ test/server.test.js
@@ -18,3 +18,29 @@ describe("/ API", () => {
       });
   });
 });
+
+describe("Check programs for Python lang", () => {
+  it("should successfully run the program", done => {
+    chai.request(server)
+      .post('/run')
+      .send({
+        "language": "python",
+        "code": "print(\"Hello\", end=\"\")",
+        "input": "Hello",
+        "eo": "Hello"
+      })
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res).to.be.json
+        expect(res).to.have.status(200)
+        expect(res.body.matches).to.be.true
+        expect(res.body.message).to.equal("Program works correctly")
+        expect(res.body.expected).to.equal("Hello")
+        expect(res.body.actual).to.equal("Hello")
+        expect(res.body.hasError).to.be.false
+        expect(res.body.outOfResources).to.be.false
+        expect(res.body.errorMessage).to.equal("")
+        done()
+      })
+  })
+});
